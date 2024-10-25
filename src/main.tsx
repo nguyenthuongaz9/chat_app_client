79f0c8dace2ab1f1f4a4104e032d87aedddb2b0e
nguyen thuong <nguyenthuongaz90@gmail.com>
2024-10-26 00:08:57 +0700

    fixed err

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,6 @@ import ModelProvider from './components/providers/ModelProvider.tsx'
 import { QueryProvider } from './components/providers/QueryProvider.tsx'
 import { SocketProvider } from './components/providers/SocketProvider.tsx'
 
-if (process.env.NODE_ENV === 'production') {
-  // Disable React DevTools
-  const { disableReactDevTools } = require('@welldone-software/disable-react-devtools');
-  disableReactDevTools();
-}
-
 
 
 createRoot(document.getElementById('root')!).render(
@@ -20,14 +14,11 @@ createRoot(document.getElementById('root')!).render(
     <>
         <SocketProvider>
           <QueryProvider>
-            
             <App />
             <Toaster />
             <ModelProvider />
-          
           </QueryProvider>     
         </SocketProvider>
-
     </>
   </StrictMode>,
 )
